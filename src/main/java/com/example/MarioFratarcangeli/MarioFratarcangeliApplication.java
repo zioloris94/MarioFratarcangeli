51ff50a0cb5f1c5c1d81cdd08f03cbc65b6794d5
lorispersonale <loris94fr@gmail.com>
2024-12-08 17:53:44 +0100

    release start

---
+++ src/main/java/com/example/MarioFratarcangeli/MarioFratarcangeliApplication.java
@@ -0,0 +1,13 @@
+package com.example.MarioFratarcangeli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarioFratarcangeliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarioFratarcangeliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MarioFratarcangeli/MarioFratarcangeliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MarioFratarcangeli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarioFratarcangeliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
